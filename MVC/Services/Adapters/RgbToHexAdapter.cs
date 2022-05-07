a11843ae2f12b3174d22b9f6c3d0e8c1474bc3e2
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2022-05-07 11:49:28 -0500

    Created adapter classes

---
+++ MVC/Services/Adapters/RgbToHexAdapter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Adapters
+{
+    //Author: Sebastian Pedersen
+    //Creation Date: May 7, 2022
+    public class RgbToHexAdapter
+    {
+    }
+}
