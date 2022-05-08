5708d2d851c9301c4866bb29ca7f53f35837b32c
sim2kid <sim2kid@users.noreply.github.com>
2022-05-07 19:36:03 -0500

    Fixed new MVC

--- MVC/MVC-Core/Controllers/HomeController.cs
+++ MVC/MVC-Core/Controllers/HomeController.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 
-namespace MVC.Controllers
+namespace MVC_Core.Controllers
 {
     public class HomeController : Controller
     {
---
+++ MVC/MVC-Core/Models/GameModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Core.Models
+{
+    public class GameModel
+    {
+        public int R { get; set; }
+        public int G { get; set; }
+        public int B { get; set; }
+    }
+}
