e4d8839118e7303f5cc36324d765f9075b5dafcc
sim2kid <sim2kid@users.noreply.github.com>
2022-05-07 19:09:51 -0500

    Added hashing on MVC

---
+++ MVC/Services/Utilities/HashUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
+
+// Written by Owen Ravelo
+
+namespace Services.Utilities
+{
+    public static class HashUtil
+    {
+        public static string HashPassword(string password) 
+        {
+            byte[] byteword = Encoding.Unicode.GetBytes(password);
+            SHA512 sha512 = SHA512.Create();
+            byteword = sha512.ComputeHash(byteword);
+
+            StringBuilder result = new StringBuilder();
+            foreach (byte b in byteword) 
+            {
+                result.Append(b.ToString("x2"));
+            }
+            return result.ToString();
+        }
+    }
+}
